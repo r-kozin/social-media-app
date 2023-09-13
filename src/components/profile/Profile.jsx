b18ebeac24084ae640f493f3aae09c17181c6110
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-09-13 12:49:02 -0700

    completed profile join date, posts, postlist functionality

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,36 +1,43 @@
 import { Divider, Flex, HStack, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
-import { PostsList } from "../posts/PostsList";
+import { PostList } from "../post/PostList";
+import { useGetPosts } from "../../hooks/posts";
+import Avatar from "./Avatar";
+import { useUser } from "../../hooks/users";
+import { format } from "date-fns";
 
 
 export const Profile = () => {
 
     const {id} = useParams();
+    const {posts, isLoading: postsLoading} = useGetPosts(id);
+    const {user, isLoading: userLoading} = useUser(id)
 
-
+    if(userLoading) return "Loading..."
 
   return (
     <Stack spacing={5}>
       <Flex p={["4", "6"]} pos={"relative"} align={"center"}>
-        Insert Avatar Here (size 2xl)
+        <Avatar size={"2xl"} user={user} />
         <Stack ml={"10"}>
-          @username Here
+          <Text fontSize={'2xl'}>{user.username}</Text>
           <HStack spacing={"10"}>
             <Text color={"gray.700"} fontSize={["sm", "lg"]}>
-              Posts: 10
+              Posts: {posts.length}
             </Text>
             <Text color={"gray.700"} fontSize={["sm", "lg"]}>
               Likes: todo!
             </Text>
             <Text color={"gray.700"} fontSize={["sm", "lg"]}>
-              Joined: Jan 01 2022
+              Joined: {format(user.date, "MMMM yyyy")}
             </Text>
           </HStack>
         </Stack>
       </Flex>
       <Divider />
-      <PostsList />
+
+      {postsLoading ? <Text>Posts are loading...</Text> : <PostList posts={posts}/>}
     </Stack>
   );
 };
--- src/hooks/posts.jsx
+++ src/hooks/posts.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
-import { doc, setDoc, query, collection, orderBy, updateDoc, arrayRemove, arrayUnion } from "firebase/firestore";
+import {
+  doc,
+  setDoc,
+  query,
+  collection,
+  orderBy,
+  updateDoc,
+  arrayRemove,
+  arrayUnion,
+  where,
+} from "firebase/firestore";
 import { db } from "../lib/firebase";
 import { uuidv4 } from "@firebase/util";
 import { useToast } from "@chakra-ui/react";
-import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore";
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
 
 export function useAddPost() {
   const [isLoading, setLoading] = useState(false);
@@ -30,8 +43,14 @@ export function useAddPost() {
   return { addPost, isLoading };
 }
 
-export function useGetPosts() {
-  const q = query(collection(db, "posts"), orderBy("date", "desc"));
+export function useGetPosts(uid = null) {
+  const q = uid
+    ? query(
+        collection(db, "posts"),
+        orderBy("date", "desc"),
+        where("uid", "==", uid)
+      )
+    : query(collection(db, "posts"), orderBy("date", "desc"));
   const [posts, isLoading, error] = useCollectionData(q);
 
   if (error) {
@@ -48,23 +67,23 @@ export function useToggleLike({ id, isLiked, uid }) {
     const postRef = doc(db, "posts", id);
     await updateDoc(postRef, {
       likes: isLiked ? arrayRemove(uid) : arrayUnion(uid),
-    })
+    });
     setLoading(false);
   }
-  return {toggleLike, isLoading}
+  return { toggleLike, isLoading };
 }
 
-export function useDeletePost({id}) {
+export function useDeletePost({ id }) {
   const [isLoading, setLoading] = useState(false);
 
   async function deletePost() {}
 
-  return {deletePost, isLoading}
+  return { deletePost, isLoading };
 }
 
-export function useGetPost({id}) {
-  const q = doc(db, "posts", id)
-  const [post, isLoading] = useDocumentData(q)
+export function useGetPost({ id }) {
+  const q = doc(db, "posts", id);
+  const [post, isLoading] = useDocumentData(q);
 
-  return {post, isLoading}
+  return { post, isLoading };
 }
